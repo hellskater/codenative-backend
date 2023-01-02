5fa9603edd7fa279f692f638ffae168a969b3a52
hellskater <ksrinivasrao531@gmail.com>
2023-01-03 03:10:33 +0530

    cors error

--- src/setup/middleware.ts
+++ src/setup/middleware.ts
@@ -4,7 +4,12 @@ import cors from "cors";
 import Router from "@api/routes";
 
 export const initializeMiddlewares = (app: any): void => {
-  app.use(cors())
+  const corsOptions = {
+    origin: "*",
+    credentials: true, //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+  };
+  app.use(cors(corsOptions));
   app.use(urlencoded({ extended: true }));
   app.use(json());
 
