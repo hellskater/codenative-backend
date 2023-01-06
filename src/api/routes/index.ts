7a2c4d1711bc2cb38ae3dde91e809be210435cf6
hellskater <ksrinivasrao531@gmail.com>
2023-01-06 11:38:28 +0530

    removed issue ip route

--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -1,11 +1,9 @@
 import express from 'express'
 
 import exampleRoutes from '@api/routes/example'
-import IpRoutes from '@api/routes/ip'
 
 const router = express.Router()
 
 router.use('/example', exampleRoutes)
-router.use('/ip', IpRoutes)
 
 export default router
